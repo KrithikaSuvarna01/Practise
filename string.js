3f9c0c388c76d6e3ebbdbf2e21256641f92eba37
hrithik-b01 <hrithik.b@codecraft.co.in>
2023-08-15 22:06:04 +0530

    feat: implemented reverse function

--- string.js
+++ string.js
@@ -1,1 +1,9 @@
 //This FIle contains js functions related to String
+/**
+ * This function reverses a string.
+ * @param {string} str 
+ * @returns the result.
+ */
+function ReverseString(str) {
+    return str.split('').reverse().join('')
+}
